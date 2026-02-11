6fabf7b351375bdeca966d4dfcbe63619e2b090f
ApornaRakshit <rakshit.aporna@gmail.com>
2026-02-11 13:34:38 +0600

    Display word and Handle empty lessons for better way

--- script/index.js
+++ script/index.js
@@ -15,14 +15,35 @@ const displayLevelWord = (words) => {
     const wordContainer = document.getElementById("word-container");
     wordContainer.innerHTML = "";
 
+    if (words.length == 0){
+        wordContainer.innerHTML = `
+        <div class="text-center bg-sky-100 col-span-full rounded-xl py-10 space-y-6 font-bangla">
+        <img class="mx-auto" src="./assets/alert-error.png" />    
+        <p class="text-xl font-medium text-gray-400">
+                এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি। 
+            </p>
+            <h2 class="font-bold text-4xl">
+                নেক্সট Lesson এ যান
+            </h2>
+        </div>
+        `;
+        return;
+    }
+
     words.forEach(word => {
-        console.log(word);
+        // console.log(word);
         const card = document.createElement("div");
         card.innerHTML = `
         <div class="bg-white rounded-lg shadow-sm text-center py-10 px-5">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
-            <p class="font-semibold">Meaning / Pronounciation</p>
-            <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronounciation}"</div>
+            <h2 class="font-bold text-2xl">${
+                word.word ? word.word: "শব্দ পাওয়া যায় নি"
+            }</h2>
+            <p class="font-semibold">Meaning / Pronunciation</p>
+            <div class="text-2xl font-medium font-bangla">"${
+                word.meaning ? word.meaning: "অর্থ পাওয়া যায়নি"
+            } / ${
+                word.pronunciation ? word.pronunciation: "pronunciation পাওয়া যায়নি"
+            }"</div>
             <div class="flex justify-between items-center p-4">
                 <button class="btn bg-sky-50 p-3 hover:bg-sky-600"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-sky-50 p-3 hover:bg-sky-600"><i class="fa-solid fa-volume-high"></i></button>
